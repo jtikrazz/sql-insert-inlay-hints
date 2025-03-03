1bee1952ac36d193703059118ae76fca2c0a1966
Johnny MA <johnny.ma@prologia.fr>
2025-03-03 09:23:24 +0100

    minify

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  target: "node",
+  mode: "production",
+  entry: "./src/extension.ts",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "extension.js",
+    libraryTarget: "commonjs2",
+  },
+  externals: {
+    vscode: "commonjs vscode",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: "ts-loader",
+      },
+    ],
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{ from: "src/icon.png", to: "." }],
+    }),
+  ],
+};
